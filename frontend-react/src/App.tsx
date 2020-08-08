e00b7beec72233f291d3127acaa1c26d709ba271
nurlimandiara <nurlimandiara@gmail.com>
2020-08-09 01:37:13 +0700

    install react-boostrasp

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          Home
-        </Route>
-        <Route path="/karyawan">
-          Karyawan
-        </Route>
-      </Switch>
+      <div className="main-container">
+        <Switch>
+          <Route path="/" exact>
+            Home
+          </Route>
+          <Route path="/karyawan">Karyawan</Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
