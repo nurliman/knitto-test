0a1c148a555c09ce9dd9654ea701198e0805b93a
nurlimandiara <nurlimandiara@gmail.com>
2020-08-08 22:10:15 +0700

    rename combineReducers.js to rootReducer.js

--- frontend-react/src/store/createStore.ts
+++ frontend-react/src/store/createStore.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './modules/combineReducers';
+import rootReducer from './modules/rootReducer';
 import reduxThunk from 'redux-thunk';
 
 export default createStore(rootReducer, applyMiddleware(reduxThunk));
