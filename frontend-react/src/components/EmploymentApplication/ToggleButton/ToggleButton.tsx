fb3ed17852da108a30e78e668a793615685eb0c4
unknown <nurlimandiara@gmail.com>
2020-08-12 17:00:21 +0700

    .

--- frontend-react/src/components/EmploymentApplication/ToggleButton/ToggleButton.tsx
+++ frontend-react/src/components/EmploymentApplication/ToggleButton/ToggleButton.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ToggleButton: React.FC = () => {
   return (
-    <div className="can-toggle demo-rebrand-1">
+    <div className="can-toggle toggle-theme-1">
       <input id="d" type="checkbox" />
       <label htmlFor="d">
         <div
           className="can-toggle__switch"
           data-checked="Hirer"
           data-unchecked="Employee"
-        ></div>
+        />
       </label>
     </div>
   );
